8556cef395785ac1ad3ffd9b5855fc5020e4ccff
Alejandro <alejandro_mr7@outlook.com>
2021-01-30 03:36:21 -0600

    Cambiandola a mysql

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 // Option 1: Passing parameters separately
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER || 'alex', process.env.BD_PASS || 'ok', {
+const db = new Sequelize(process.env.BD_NOMBRE || 'nodejs', process.env.BD_USER || 'alex', process.env.BD_PASS || 'ok', {
   host: process.env.BD_HOST || '127.0.0.1',
   dialect: 'mysql',
+  port: process.env.BD_PORT || 3306, 
   operatsAliases:false,
 
   define: {
