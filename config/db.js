cd9240306fe74312c09726df71d02f7932b8990f
Alejandro <alejandro_mr7@outlook.com>
2021-01-30 02:56:46 -0600

    desplegandola en heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config()
+
 // Option 1: Passing parameters separately
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER || 'alex', process.env.BD_PASS || 'ok', {
   host: process.env.BD_HOST || '127.0.0.1',
   dialect: 'mssql',
   operatsAliases:false,
